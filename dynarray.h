40d7265cc6c70d98acd6cf3761348d5964e9dc3b
gmd01bit <gmd01bit@gmail.com>
2023-12-27 01:29:50 +0200

    :sparkles: Add function and type methods

--- dynarray.h
+++ dynarray.h
@@ -10,12 +10,18 @@
 #define dynarray_init_fn(type_t) CONCAT(dynarray_init_, type_t)
 #define dynarray_deinit_fn(type_t) CONCAT(dynarray_deinit_, type_t)
 #define dynarray_reserve_fn(type_t) CONCAT(dynarray_reserve_, type_t)
+#define dynarray_add_fn(type_t) CONCAT(dynarray_add_, type_t)
 
 #define dynarray_init(type, sz) dynarray_init_fn(type)(sz)
+#define dynarray_associated(dynarray_fn, array_ref, ...) \
+	GEN_TYPE(dynarray_fn, *(array_ref)->ptr) \
+	(array_ref __VA_OPT__(, ) __VA_ARGS__)
 #define dynarray_deinit(array_ref) \
-	GEN_TYPE(dynarray_deinit_fn, *(array_ref)->ptr)(array_ref)
+	dynarray_associated(dynarray_deinit_fn, array_ref)
 #define dynarray_reserve(array_ref, sz) \
-	GEN_TYPE(dynarray_reserve_fn, *(array_ref)->ptr)(array_ref, sz)
+	dynarray_associated(dynarray_reserve_fn, array_ref, sz)
+#define dynarray_add(array_ref, elt) \
+	dynarray_associated(dynarray_add_fn, array_ref, elt)
 
 #define dynarray_declare(type_t) \
 	dynarray_t(type_t) { \
@@ -27,7 +33,9 @@
 		      STRING(type_t) " dynarray"); \
 	DECLARE(dynarray_t(type_t), dynarray_init_fn(type_t), size_t) \
 	DECLARE(void, dynarray_deinit_fn(type_t), dynarray_t(type_t) *) \
-	DECLARE(void, dynarray_reserve_fn(type_t), dynarray_t(type_t) *, size_t)
+	DECLARE(void, dynarray_reserve_fn(type_t), dynarray_t(type_t) *, \
+		size_t) \
+	DECLARE(void, dynarray_add_fn(type_t), dynarray_t(type_t) *, type_t)
 
 #define dynarray_define(type_t) \
 	dynarray_t(type_t) dynarray_init_fn(type_t)(size_t size) { \
@@ -43,13 +51,19 @@
 		*array = (dynarray_t(type_t)){0}; \
 	} \
 	void dynarray_reserve_fn(type_t)(dynarray_t(type_t) * array, \
-					size_t capacity) { \
+					 size_t capacity) { \
 		if (capacity <= array->capacity) return; \
 		*array = (dynarray_t(type_t)){ \
 		    .ptr = realloc(array->ptr, capacity * sizeof(type_t)), \
 		    .size = array->size, \
 		    .capacity = capacity, \
 		}; \
+	} \
+	void dynarray_add_fn(type_t)(dynarray_t(type_t) * array, \
+				      type_t elt) { \
+		if (array->capacity == 0) array->capacity = 1; \
+		dynarray_reserve_fn(type_t)(array, 2 * array->capacity); \
+		array->ptr[array->size++] = elt; \
 	}
 
 #endif // !GEN_DYNARRAY
--- test/unittest.c
+++ test/unittest.c
@@ -8,13 +8,20 @@
 
 #endif /* ifndef UNITTEST_MAIN */
 
-unittest("init reserve deinit") {
+unittest("init reserve add deinit") {
 	dynarray_t(int) array = dynarray_init(int, 1);
 	ensure(array.ptr != NULL);
+	ensure(array.size == 1);
 	array.ptr[0] = 42;
+	array.size = 0;
 	dynarray_reserve(&array, 10);
 	ensure(array.ptr != NULL);
-	array.ptr[9] = 42;
+
+	for (size_t i = 0; i != 10; ++i) {
+		dynarray_add(&array, i + 1);
+	}
+	ensure(array.size == 10);
+
 	dynarray_deinit(&array);
 	ensure(array.ptr == NULL);
 }
